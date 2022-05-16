65d5246a7867ad4eeb3a07d25c122d98d42d5406
wendemsky <61985456+wendemsky@users.noreply.github.com>
2022-05-16 16:40:03 +0530

    Added the Dice Game project

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1; //1-6
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1; //1-6
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //images/dice1.png - images/dice6.png
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins! 👑";
+}else if(randomNumber1<randomNumber2) document.querySelector("h1").innerHTML = "Player 2 Wins! 👑";
+else document.querySelector("h1").innerHTML = "Draw!";
